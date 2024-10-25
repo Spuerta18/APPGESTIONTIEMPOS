b5d0b9ff3b457198fbb96885f7886965e1a94232
Spuerta18 <santiagopuerta1996@gmail.com>
2024-10-24 20:37:01 -0500

    se crea estructura basica de viewproyectos

---
+++ controllers/Controllerproyectos.js
@@ -0,0 +1,15 @@
+import { proyectos } from "../models/ModelProyectos.js";
+
+
+export function listarProyectos(){
+
+}
+export function filtrarPorEmpresas(){
+
+}
+export function filtrarPorProyectos(){
+
+}
+export function filtrarPorTipoProyecto(){
+
+}
